5a4266f660b65f9573cb41ce52127653c8d42891
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-12-19 13:14:13 -0300

    feat: restrict movement on the screen

--- main.py
+++ main.py
@@ -74,13 +74,13 @@ def main():
         run = False
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]: # Left
+    if keys[pygame.K_a] and ship.x - player_vel > 0: # Left
       ship.x -= player_vel
-    if keys[pygame.K_d]: # Right
+    if keys[pygame.K_d] and ship.x + player_vel + 50 < WIDTH: # Right
       ship.x += player_vel
-    if keys[pygame.K_w]: # Up
+    if keys[pygame.K_w] and ship.y - player_vel > 0: # Up
       ship.y -= player_vel
-    if keys[pygame.K_s]: # Down
+    if keys[pygame.K_s] and ship.y + player_vel + 50 < HEIGHT: # Down
       ship.y += player_vel
 
 main()
